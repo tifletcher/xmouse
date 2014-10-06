8275a710ad272c05aa413db7101896be6167a3ce
Themba Fletcher <tfletcher@frontporch.com>
2014-10-06 15:51:04 -0700

    working

--- xmouse/Program.cs
+++ xmouse/Program.cs
@@ -10,99 +10,80 @@ namespace xmouse
 	{
 
 		private const string User32 = "user32.dll";
-		private const string SPIExceptionMessage = "Error calling SystemParametersInfo";
-
-		private static class External
+		public enum Actions
 		{
-			public enum Actions
-			{
-				//Determines whether active window tracking (activating the window the mouse is on) is on or off. The pvParam parameter must point to a BOOL variable that receives TRUE for on, or FALSE for off.
-				SPI_GETACTIVEWINDOWTRACKING = 0x1000,
+			//Determines whether active window tracking (activating the window the mouse is on) is on or off. The pvParam parameter must point to a BOOL variable that receives TRUE for on, or FALSE for off.
+			SPI_GETACTIVEWINDOWTRACKING = 0x1000,
 
-				//Determines whether active window tracking (activating the window the mouse is on) is on or off. The pvParam parameter must point to a BOOL variable that receives TRUE for on, or FALSE for off.		//Determines whether windows activated through active window tracking will be brought to the top. The pvParam parameter must point to a BOOL variable that receives TRUE for on, or FALSE for off.
-				SPI_GETACTIVEWNDTRKZORDER = 0x100C,
+			//Determines whether active window tracking (activating the window the mouse is on) is on or off. The pvParam parameter must point to a BOOL variable that receives TRUE for on, or FALSE for off.		//Determines whether windows activated through active window tracking will be brought to the top. The pvParam parameter must point to a BOOL variable that receives TRUE for on, or FALSE for off.
+			SPI_GETACTIVEWNDTRKZORDER = 0x100C,
 
-				// Retrieves the active window tracking delay, in milliseconds. The pvParam parameter must point to a DWORD variable that receives the time.
-				SPI_GETACTIVEWNDTRKTIMEOUT = 0x2002,
+			// Retrieves the active window tracking delay, in milliseconds. The pvParam parameter must point to a DWORD variable that receives the time.
+			SPI_GETACTIVEWNDTRKTIMEOUT = 0x2002,
 
-				//Sets active window tracking (activating the window the mouse is on) either on or off. Set pvParam to TRUE for on or FALSE for off.
-				SPI_SETACTIVEWINDOWTRACKING = 0x1001,
+			//Sets active window tracking (activating the window the mouse is on) either on or off. Set pvParam to TRUE for on or FALSE for off.
+			SPI_SETACTIVEWINDOWTRACKING = 0x1001,
 
-				//Determines whether or not windows activated through active window tracking should be brought to the top. Set pvParam to TRUE for on or FALSE for off.
-				SPI_SETACTIVEWNDTRKZORDER = 0x100D,
+			//Determines whether or not windows activated through active window tracking should be brought to the top. Set pvParam to TRUE for on or FALSE for off.
+			SPI_SETACTIVEWNDTRKZORDER = 0x100D,
 
-				//Sets the active window tracking delay. Set pvParam to the number of milliseconds to delay before activating the window under the mouse pointer.
-				SPI_SETACTIVEWNDTRKTIMEOUT = 0x2003,
-			}
-
-			[DllImport(User32, CharSet = CharSet.Auto)]
-			public static extern bool SystemParametersInfo(Actions uiAction, int uiParam, ref bool value, int ignore);
-
-
-			[DllImport(User32, CharSet = CharSet.Auto)]
-			public static extern bool SystemParametersInfo(Actions nAction, int nParam, ref DWORD value, int ignore);
-
+			//Sets the active window tracking delay. Set pvParam to the number of milliseconds to delay before activating the window under the mouse pointer.
+			SPI_SETACTIVEWNDTRKTIMEOUT = 0x2003,
 		}
 
-		private static void SystemParametersInfo(External.Actions action, int param, ref bool value, int ignore)
-		{
-			Console.WriteLine(value);
+		[DllImport(User32, CharSet = CharSet.Auto)]
+		public static extern bool SystemParametersInfo(Actions uiAction, int uiParam, ref bool value, int ignore);
 
 
+		[DllImport(User32, CharSet = CharSet.Auto)]
+		public static extern bool SystemParametersInfo(Actions nAction, int nParam, IntPtr value, int ignore);
 
-			bool extValue = value;
-			if (!External.SystemParametersInfo(action, param, ref extValue, ignore )) throw new Exception(SPIExceptionMessage);
-			value = extValue;
+		[DllImport(User32, CharSet = CharSet.Auto)]
+		public static extern bool SystemParametersInfo(Actions nAction, int nParam, ref Int32 value, int ignore);
 
-
-			Console.WriteLine(value);
-
-		}
-
-		private static void SystemParametersInfo(External.Actions action, int param, ref DWORD value, int ignore)
-		{
-			Console.WriteLine(value);
-
-
-			DWORD extValue = value;
-			if (!External.SystemParametersInfo(action, param, ref value, ignore )) throw new Exception(SPIExceptionMessage);
-			value = extValue;
-
-
-
-			Console.WriteLine(value);
-		}
 
 		public static bool Enabled
 		{
 			get
 			{
 				bool enabled = false;
-				SystemParametersInfo(External.Actions.SPI_GETACTIVEWINDOWTRACKING, 0, ref enabled, 0);
+				SystemParametersInfo(Actions.SPI_GETACTIVEWINDOWTRACKING, 0, ref enabled, 0);
 				return enabled;
 			}
-			set { SystemParametersInfo(External.Actions.SPI_SETACTIVEWINDOWTRACKING, 0, ref value, 1); }
+			set
+			{
+				var enabled = new IntPtr((value) ? 1 : 0);
+				SystemParametersInfo(Actions.SPI_SETACTIVEWINDOWTRACKING, 0, enabled, 1);
+			}
 		}
 
 		public static bool RaiseOnFocus {
 			get
 			{
 				bool raise = false;
-				SystemParametersInfo(External.Actions.SPI_GETACTIVEWNDTRKZORDER, 0, ref raise, 0);
+				SystemParametersInfo(Actions.SPI_GETACTIVEWNDTRKZORDER, 0, ref raise, 0);
 				return raise;
 			}
-			set { SystemParametersInfo(External.Actions.SPI_SETACTIVEWNDTRKZORDER, 0, ref value, 1); }
+			set
+			{
+				var raise = new IntPtr((value) ? 1 : 0);
+				SystemParametersInfo(Actions.SPI_SETACTIVEWNDTRKZORDER, 0, raise, 1);
+			}
 		}
 
-		public static UInt32 Timeout
+		public static Int32 Timeout
 		{
 			get
 			{
-				DWORD timeout = 0;
-				SystemParametersInfo(External.Actions.SPI_GETACTIVEWNDTRKTIMEOUT, 0, ref timeout, 0);
+				Int32 timeout = 0;
+				SystemParametersInfo(Actions.SPI_GETACTIVEWNDTRKTIMEOUT, 0, ref timeout, 0);
 				return timeout;
 			}
-			set { SystemParametersInfo(External.Actions.SPI_SETACTIVEWNDTRKTIMEOUT, 0, ref value, 1); }
+			set
+			{
+				var timeout = new IntPtr(value);
+				SystemParametersInfo(Actions.SPI_SETACTIVEWNDTRKTIMEOUT, 0, timeout, 1);
+			}
 		}
 	}
 
@@ -126,12 +107,12 @@ namespace xmouse
 
 		static void Main(string[] args)
 		{
-			//ActiveWindowTracking.Timeout = 128;
-			//ActiveWindowTracking.Enabled = true;
+			ActiveWindowTracking.Timeout = 128;
+			ActiveWindowTracking.Enabled = true;
+			ActiveWindowTracking.RaiseOnFocus = false;
 
 			Report();
 			Console.ReadKey();
-
 		}
 
 		#region copypasta docs
@@ -165,45 +146,4 @@ namespace xmouse
 		// ... static extern etc
 		#endregion
 	}
-
-	//static class Temp
-	//{
-	//	[DllImport("user32.dll", CharSet = CharSet.Auto)]
-	//	public static extern bool SystemParametersInfo(Actions nAction, int nParam, ref bool value, int ignore);
-
-
-	//	public enum Actions
-	//	{
-	//		//Determines whether active window tracking (activating the window the mouse is on) is on or off. The pvParam parameter must point to a BOOL variable that receives TRUE for on, or FALSE for off.
-	//		SPI_GETACTIVEWINDOWTRACKING = 0x1000,
-
-	//		//Determines whether active window tracking (activating the window the mouse is on) is on or off. The pvParam parameter must point to a BOOL variable that receives TRUE for on, or FALSE for off.		//Determines whether windows activated through active window tracking will be brought to the top. The pvParam parameter must point to a BOOL variable that receives TRUE for on, or FALSE for off.
-	//		SPI_GETACTIVEWNDTRKZORDER = 0x100C,
-
-	//		// Retrieves the active window tracking delay, in milliseconds. The pvParam parameter must point to a DWORD variable that receives the time.
-	//		SPI_GETACTIVEWNDTRKTIMEOUT = 0x2002,
-
-	//		//Sets active window tracking (activating the window the mouse is on) either on or off. Set pvParam to TRUE for on or FALSE for off.
-	//		SPI_SETACTIVEWINDOWTRACKING = 0x1001,
-
-	//		//Determines whether or not windows activated through active window tracking should be brought to the top. Set pvParam to TRUE for on or FALSE for off.
-	//		SPI_SETACTIVEWNDTRKZORDER = 0x100D,
-
-	//		//Sets the active window tracking delay. Set pvParam to the number of milliseconds to delay before activating the window under the mouse pointer.
-	//		SPI_SETACTIVEWNDTRKTIMEOUT = 0x2003,
-	//	}
-
-	//	public static bool Enabled
-	//	{
-	//		get
-	//		{
-	//			bool enabled = false;
-	//			SystemParametersInfo(Actions.SPI_GETACTIVEWINDOWTRACKING, 0, ref enabled, 0);
-	//			return enabled;
-	//		}
-	//		set { SystemParametersInfo(Actions.SPI_SETACTIVEWINDOWTRACKING, 0, ref value, 1); }
-	//	}
-	//}
-
-
 }
